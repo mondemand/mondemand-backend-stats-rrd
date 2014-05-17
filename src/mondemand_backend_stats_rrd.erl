1bf89491e50253472adc0c0721cdccc1d8d923c0
Anthony Molinaro <anthony.molinaro@openx.com>
2014-05-17 15:11:30 -0700

    implement new behaviour

--- src/mondemand_backend_stats_rrd.erl
+++ src/mondemand_backend_stats_rrd.erl
@@ -1,16 +1,14 @@
 -module (mondemand_backend_stats_rrd).
 
--behaviour (gen_server).
+-behaviour (supervisor).
 -behaviour (mondemand_server_backend).
 -behaviour (mondemand_backend_stats_handler).
-
--export ([ update_cache/0 ]).
 
 %% mondemand_server_backend callbacks
 -export ([ start_link/1,
            process/1,
-           stats/0,
-           required_apps/0
+           required_apps/0,
+           type/0
          ]).
 
 %% mondemand_backend_stats_handler callbacks
@@ -21,78 +19,63 @@
            handle_response/2
          ]).
 
-%% gen_server callbacks
--export ([ init/1,
-           handle_call/3,
-           handle_cast/2,
-           handle_info/2,
-           terminate/2,
-           code_change/3
-         ]).
-
--record (state, { sidejob,
-                  stats = dict:new ()
-                }).
--define (TABLE, md_be_stats_rrd_filecache).
+%% supervisor callbacks
+-export ([init/1]).
 
 %%====================================================================
 %% mondemand_server_backend callbacks
 %%====================================================================
 start_link (Config) ->
-  gen_server:start_link ( { local, ?MODULE }, ?MODULE, Config, []).
+  supervisor:start_link ({local, ?MODULE}, ?MODULE, [Config]).
 
 process (Event) ->
-  case mondemand_backend_connection_pool:cast (?MODULE, {process, Event}) of
-    overload ->
-      error_logger:info_msg ("overloaded~n",[]),
-      overload;
-    O ->
-      O
-  end.
-
-stats () ->
-  gen_server:call (?MODULE, {stats}).
+  mondemand_backend_worker_pool_sup:process
+    (mondemand_backend_stats_rrd_worker_pool, Event).
 
 required_apps () ->
-  [ sidejob, afunix, erlrrd ].
+  [ erlrrd ].
+
+type () ->
+  supervisor.
 
 %%====================================================================
-%% gen_server callbacks
+%% supervisor callbacks
 %%====================================================================
-init (Config) ->
-  Limit = proplists:get_value (limit, Config, 10),
-  Number = proplists:get_value (number, Config, undefined),
+init ([Config]) ->
   Prefix = proplists:get_value (prefix, Config, "."),
 
   mondemand_server_util:mkdir_p (Prefix),
 
-  ets:new (?TABLE, [set, public, named_table, {keypos, 1}]),
+  Number = proplists:get_value (number, Config, 16), % FIXME: replace default
 
-  { ok, Proc } =
-    mondemand_backend_connection_pool:init ([?MODULE, Limit, Number]),
-  { ok, #state { sidejob = Proc } }.
-
-handle_call ({stats}, _From, State) ->
-  Stats = mondemand_backend_connection_pool:stats (?MODULE),
-  { reply, Stats, State };
-handle_call (Request, From, State) ->
-  error_logger:warning_msg ("~p : Unrecognized call ~p from ~p~n",
-                            [?MODULE, Request, From]),
-  { reply, ok, State }.
-
-handle_cast (Request, State) ->
-  error_logger:warning_msg ("~p : Unrecognized cast ~p~n",[?MODULE, Request]),
-  { noreply, State }.
-
-handle_info (Request, State) ->
-  error_logger:warning_msg ("~p : Unrecognized info ~p~n",[?MODULE, Request]),
-  {noreply, State}.
-
-terminate (_Reason, #state { }) ->
-  ok.
-
-code_change (_OldVsn, State, _Extra) ->
-  {ok, State}.
+  { ok,
+    {
+      {one_for_one, 10, 10},
+      [
+        { mondemand_backend_stats_rrd_filecache,
+          { mondemand_backend_stats_rrd_filecache, start_link,
+            []
+          },
+          permanent,
+          2000,
+          worker,
+          [ mondemand_backend_stats_rrd_filecache ]
+        },
+        { mondemand_backend_stats_rrd_worker_pool,
+          { mondemand_backend_worker_pool_sup, start_link,
+            [ mondemand_backend_stats_rrd_worker_pool,
+              mondemand_backend_connection,
+              Number,
+              ?MODULE ]
+          },
+          permanent,
+          2000,
+          supervisor,
+          [ ]
+        }
+      ]
+    }
+  }.
 
 %%====================================================================
 %% mondemand_backend_stats_handler callbacks
@@ -105,7 +88,8 @@ format_stat (Prefix, ProgId, Host,
              MetricType, MetricName, MetricValue, Timestamp, Context) ->
 
   RRDFilePath =
-    check_cache (Prefix,ProgId,MetricType,MetricName,Host,Context),
+    mondemand_backend_stats_rrd_filecache:check_cache
+      (Prefix,ProgId,MetricType,MetricName,Host,Context),
   case RRDFilePath of
     error -> error;
     {ok, P} ->
@@ -116,103 +100,6 @@ footer () -> "\n.\n".
 
 handle_response (Response, Previous) ->
   parse_response (Response, Previous).
-
-check_cache (Prefix, ProgId, MetricType, MetricName, Host, Context) ->
-  FileKey = {ProgId,MetricType,MetricName,Host,Context},
-  case ets:lookup (?TABLE, FileKey) of
-    [{_, FP}] ->
-      {ok, FP};
-    [] ->
-      ContextString =
-        case Context of
-          [] -> "";
-          L -> [ "-",
-                 mondemand_server_util:join ([[K,"=",V] || {K, V} <- L ], "-")
-               ]
-        end,
-
-      FileName = list_to_binary ([ProgId,
-                                  "-",MetricType,
-                                  "-",MetricName,
-                                  "-",Host,
-                                  ContextString,
-                                  ".rrd"]),
-      FilePath =
-        filename:join([Prefix,
-                       ProgId,
-                       MetricName]),
-
-      mondemand_server_util:mkdir_p (FilePath),
-
-      RRDFile = filename:join ([FilePath, FileName]),
-      case maybe_create (MetricType, RRDFile) of
-        {ok, _} ->
-          ets:insert (?TABLE, {FileKey, RRDFile}),
-          {ok, RRDFile};
-        {timeout, Timeout} ->
-          error_logger:error_msg (
-            "Unable to create '~p' because of timeout ~p",[RRDFile, Timeout]),
-          error;
-        {error, Error} ->
-          error_logger:error_msg (
-            "Unable to create '~p' because of ~p",[RRDFile, Error]),
-          error;
-        Unknown ->
-          error_logger:error_msg (
-            "Unable to create '~p' because of unknown ~p",[RRDFile, Unknown]),
-          error
-      end
-  end.
-
-update_cache () ->
-  % sometimes RRD's will be deleting if they are no longer being accessed,
-  % in those cases we want to remove it from the cache, so this will do
-  % that by maybe recreating it
-  ToDelete =
-    ets:foldl (fun ({Key,File}, Accum) ->
-                 case file:read_file_info (File) of
-                   {ok, _} -> Accum;  % already exists so keep it
-                   _ -> [Key|Accum]   % doesn't so add to the to delete list
-                 end
-               end,
-               [],
-               ?TABLE),
-  % actually peform the deletes
-  [ ets:delete (?TABLE, K) || K <- ToDelete ],
-  ok.
-
-maybe_create (Type, File) ->
-  case file:read_file_info (File) of
-    {ok, I} -> {ok, I};
-    _ ->
-      case Type of
-        <<"counter">> -> create_counter (File);
-        <<"gauge">> -> create_gauge (File);
-        _ -> create_counter (File) % default is counter
-      end
-  end.
-
-create_counter (File) ->
-  erlrrd:create ([
-      io_lib:fwrite ("~s",[File]),
-      " --step \"60\""
-      " --start \"now - 90 days\""
-      " \"DS:value:DERIVE:900:0:U\""
-      " \"RRA:AVERAGE:0.5:1:44640\""
-      " \"RRA:AVERAGE:0.5:15:9600\""
-      " \"RRA:AVERAGE:0.5:1440:400\""
-    ]).
-
-create_gauge (File) ->
-  erlrrd:create ([
-      io_lib:fwrite ("~s",[File]),
-      " --step \"60\""
-      " --start \"now - 90 days\""
-      " \"DS:value:GAUGE:900:U:U\""
-      " \"RRA:AVERAGE:0.5:1:44640\""
-      " \"RRA:AVERAGE:0.5:15:9600\""
-      " \"RRA:AVERAGE:0.5:1440:400\""
-    ]).
 
 
 -record (parse_state, {errors = 0, expected = 0, remaining}).
---
+++ src/mondemand_backend_stats_rrd_filecache.erl
@@ -0,0 +1,156 @@
+-module (mondemand_backend_stats_rrd_filecache).
+
+-behaviour (gen_server).
+
+%% API
+-export ([ start_link/0,
+           update_cache/0,
+           check_cache/6 ]).
+
+%% gen_server callbacks
+-export ([ init/1,
+           handle_call/3,
+           handle_cast/2,
+           handle_info/2,
+           terminate/2,
+           code_change/3
+         ]).
+
+-record (state, { delay, interval }).
+-define (TABLE, md_be_stats_rrd_filecache).
+
+start_link () ->
+  gen_server:start_link ({local, ?MODULE},?MODULE,[],[]).
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+init ([]) ->
+  Interval = 3600,         % flush each hour
+  Delay = Interval * 1000, % delay is in milliseconds
+  ets:new (?TABLE, [set, public, named_table, {keypos, 1}]),
+  { ok, #state { delay = Delay, interval = Interval }, Delay }.
+
+handle_call (Request, From, State = #state { delay = Delay }) ->
+  error_logger:warning_msg ("~p : Unrecognized call ~p from ~p~n",
+                            [?MODULE, Request, From]),
+  { reply, ok, State, Delay }.
+
+handle_cast (Request, State = #state { delay = Delay }) ->
+  error_logger:warning_msg ("~p : Unrecognized cast ~p~n",[?MODULE, Request]),
+  { noreply, State, Delay }.
+
+handle_info (timeout, State = #state { delay = Delay }) ->
+  update_cache (),
+  { noreply, State, Delay };
+handle_info (Request, State = #state { delay = Delay }) ->
+  error_logger:warning_msg ("~p : Unrecognized info ~p~n",[?MODULE, Request]),
+  { noreply, State, Delay }.
+
+terminate (_Reason, #state { }) ->
+  ok.
+
+code_change (_OldVsn, State, _Extra) ->
+  { ok, State }.
+
+%% API
+check_cache (Prefix, ProgId, MetricType, MetricName, Host, Context) ->
+  FileKey = {ProgId,MetricType,MetricName,Host,Context},
+  case ets:lookup (?TABLE, FileKey) of
+    [{_, FP}] ->
+      {ok, FP};
+    [] ->
+      ContextString =
+        case Context of
+          [] -> "";
+          L -> [ "-",
+                 mondemand_server_util:join ([[K,"=",V] || {K, V} <- L ], "-")
+               ]
+        end,
+
+      FileName = list_to_binary ([ProgId,
+                                  "-",MetricType,
+                                  "-",MetricName,
+                                  "-",Host,
+                                  ContextString,
+                                  ".rrd"]),
+      FilePath =
+        filename:join([Prefix,
+                       ProgId,
+                       MetricName]),
+
+      % TODO: check for error and don't create if it's there
+      mondemand_server_util:mkdir_p (FilePath),
+
+      RRDFile = filename:join ([FilePath, FileName]),
+      case maybe_create (MetricType, RRDFile) of
+        {ok, _} ->
+          ets:insert (?TABLE, {FileKey, RRDFile}),
+          {ok, RRDFile};
+        {timeout, Timeout} ->
+          error_logger:error_msg (
+            "Unable to create '~p' because of timeout ~p",[RRDFile, Timeout]),
+          error;
+        {error, Error} ->
+          error_logger:error_msg (
+            "Unable to create '~p' because of ~p",[RRDFile, Error]),
+          error;
+        Unknown ->
+          error_logger:error_msg (
+            "Unable to create '~p' because of unknown ~p",[RRDFile, Unknown]),
+          error
+      end
+  end.
+
+update_cache () ->
+  error_logger:info_msg ("Flushing File Name Cache"),
+  % sometimes RRD's will be deleting if they are no longer being accessed,
+  % in those cases we want to remove it from the cache, so this will do
+  % that by maybe recreating it
+  ToDelete =
+    ets:foldl (fun ({Key,File}, Accum) ->
+                 case file:read_file_info (File) of
+                   {ok, _} -> Accum;  % already exists so keep it
+                   _ -> [Key|Accum]   % doesn't so add to the to delete list
+                 end
+               end,
+               [],
+               ?TABLE),
+  % actually peform the deletes
+  [ ets:delete (?TABLE, K) || K <- ToDelete ],
+  ok.
+
+maybe_create (Type, File) ->
+  case file:read_file_info (File) of
+    {ok, I} -> {ok, I};
+    _ ->
+      case Type of
+        <<"counter">> -> create_counter (File);
+        <<"gauge">> -> create_gauge (File);
+        _ -> create_counter (File) % default is counter
+      end
+  end.
+
+create_counter (File) ->
+  erlrrd:create ([
+      io_lib:fwrite ("~s",[File]),
+      " --step \"60\""
+      " --start \"now - 90 days\""
+      " \"DS:value:DERIVE:900:0:U\""
+      " \"RRA:AVERAGE:0.5:1:44640\""
+      " \"RRA:AVERAGE:0.5:15:9600\""
+      " \"RRA:AVERAGE:0.5:1440:400\""
+    ]).
+
+create_gauge (File) ->
+  erlrrd:create ([
+      io_lib:fwrite ("~s",[File]),
+      " --step \"60\""
+      " --start \"now - 90 days\""
+      " \"DS:value:GAUGE:900:U:U\""
+      " \"RRA:AVERAGE:0.5:1:44640\""
+      " \"RRA:AVERAGE:0.5:15:9600\""
+      " \"RRA:AVERAGE:0.5:1440:400\""
+    ]).
+
+
